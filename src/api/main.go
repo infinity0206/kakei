6718016b31656cd8c21e95835239b5ae436a30ed
izumihara <infinity0206.tab@gmail.com>
2021-06-12 14:17:25 +0900

    POSTリクエストができるように

--- src/api/main.go
+++ src/api/main.go
@@ -6,6 +6,7 @@ import (
   "time"
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/mysql"
+  "net/http"
 )
 
 func GetDBConn() *gorm.DB {
@@ -51,6 +52,13 @@ type Task struct {
   Assignee Assignee `gorm:"foreignKey:AssigneeRefer"`
 }
 
+
+type JsonRequest struct {
+	FieldStr  string `json:"field_str"`
+	FieldInt  int    `json:"field_int"`
+	FieldBool bool   `json:"field_bool"`
+}
+
 func main() {
   db := GetDBConn()
 
@@ -60,16 +68,25 @@ func main() {
 
   r := gin.Default()
   r.GET("/tasks", func(c *gin.Context) {
-      c.JSON(200, gin.H{
+      c.JSON(http.StatusOK, gin.H{
           "message": "tasks",
       })
   })
 
 	r.GET("/asignees", func(c *gin.Context) {
-    c.JSON(200, gin.H{
+    c.JSON(http.StatusOK, gin.H{
         "message": "asignees",
     })
   })
+
+  r.POST("/asignees", func(c *gin.Context) {
+		var json JsonRequest
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"str": json.FieldStr, "int": json.FieldInt, "bool": json.FieldBool})
+	})
     
   r.Run(":3001")  // EXPOSE Ports
 }
